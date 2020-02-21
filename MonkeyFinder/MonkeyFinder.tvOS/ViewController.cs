e5ac0c4d4c75b21ebd3c46135731dd8263e1d9a7
James Montemagno <james.montemagno@gmail.com>
2020-02-21 13:48:21 -0800

    Added MonkeyTV app!

---
+++ MonkeyFinder/MonkeyFinder.tvOS/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using SDWebImage;
+using MonkeyFinder.ViewModel;
+
+namespace MonkeyTV
+{
+    public partial class ViewController : UIViewController
+    {
+
+        MonkeysViewModel viewModel;
+        Random random = new Random();
+        public ViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            viewModel = new MonkeysViewModel();
+            ButtonGetMonkeys.PrimaryActionTriggered += ButtonGetMonkeys_PrimaryActionTriggered;
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        async private void ButtonGetMonkeys_PrimaryActionTriggered(object sender, EventArgs e)
+        {
+            ProgressBarMonkeys.StartAnimating();
+            if (viewModel.Monkeys.Count == 0)
+                await viewModel.GetMonkeysAsync();
+
+            if (viewModel.Monkeys.Count == 0)
+                return;
+
+            var next = random.Next(0, viewModel.Monkeys.Count);
+            var url = NSUrl.FromString(viewModel.Monkeys[next].Image);
+
+            ImageMonkey.SetImage(url);
+
+            ProgressBarMonkeys.StopAnimating();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
