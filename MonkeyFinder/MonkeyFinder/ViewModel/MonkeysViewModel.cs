fc4ef0fc604bcd467331e1c04014ad4f6b5e3490
Maddy Leger <maleger@microsoft.com>
2020-02-21 18:54:54 -0500

    start of demo mode with everything commented out

--- MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -34,18 +34,27 @@ namespace MonkeyFinder.ViewModel
             try
             {
                 IsBusy = true;
+                //Monkey[] monkeys = null;
 
+                //var connection = Connectivity.NetworkAccess;
 
-                // If having internet issues, read from disk
-                //string json = null;
-                //var a = System.Reflection.Assembly.GetExecutingAssembly();
-                //using (var resFilestream = a.GetManifestResourceStream("MonkeyFinder.monkeydata.json"))
+                //// if internet is working
+                //if (connection == NetworkAccess.Internet)
                 //{
-                //    using (var reader = new System.IO.StreamReader(resFilestream))
-                //        json = await reader.ReadToEndAsync();
+                //    var json = await Client.GetStringAsync("https://montemagno.com/monkeys.json");
+
+                //    monkeys = Monkey.FromJson(json);
+                //}
+                //else
+                //{
+                //    monkeys = new Monkey[]
+                //    {
+                //        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" },
+                //        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" },
+                //        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" }
+                //    };
                 //}
 
-                // if internet is working
                 var json = await Client.GetStringAsync("https://montemagno.com/monkeys.json");
 
                 var monkeys = Monkey.FromJson(json);
