b4be4bd7b84cf30412ae011b30ccda0fc42a91f2
Maddy Leger <maleger@microsoft.com>
2020-02-21 15:19:32 -0500

    initial commit of monkeys

---
+++ MonkeyFinder/MonkeyFinder/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MonkeyFinder.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MonkeyFinder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = (Color)Resources["Primary"],
+                BarTextColor = Color.White
+            };
+        }
+
+        const string AppCenteriOS = "";
+        const string AppCenterAndroid = "";
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MonkeyFinder/MonkeyFinder/Model/Monkey.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+
+namespace MonkeyFinder.Model
+{
+    public partial class Monkey
+    {
+        //[JsonProperty("Name")]
+        public string Name { get; set; }
+
+        //[JsonProperty("Location")]
+        public string Location { get; set; }
+
+        //[JsonProperty("Details")]
+        public string Details { get; set; }
+
+        //[JsonProperty("Image")]
+        public string Image { get; set; }
+
+        //[JsonProperty("Population")]
+        public long Population { get; set; }
+
+        //[JsonProperty("Latitude")]
+        public double Latitude { get; set; }
+
+        //[JsonProperty("Longitude")]
+        public double Longitude { get; set; }
+    }
+    
+    public partial class Monkey
+    {
+        public static Monkey[] FromJson(string json) => JsonSerializer.Deserialize<Monkey[]>(json);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Monkey[] self) => JsonSerializer.Serialize(self);
+    }
+}
