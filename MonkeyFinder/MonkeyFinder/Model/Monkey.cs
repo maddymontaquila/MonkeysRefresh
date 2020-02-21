3f1cb10acc27e5e10b8ba566b661359e6f9d3081
Maddy Leger <maleger@microsoft.com>
2020-02-21 15:20:03 -0500

    initial monkeys commit

--- MonkeyFinder/MonkeyFinder/Model/Monkey.cs
+++ MonkeyFinder/MonkeyFinder/Model/Monkey.cs
@@ -6,25 +6,18 @@ namespace MonkeyFinder.Model
 {
     public partial class Monkey
     {
-        //[JsonProperty("Name")]
         public string Name { get; set; }
 
-        //[JsonProperty("Location")]
         public string Location { get; set; }
 
-        //[JsonProperty("Details")]
         public string Details { get; set; }
 
-        //[JsonProperty("Image")]
         public string Image { get; set; }
 
-        //[JsonProperty("Population")]
         public long Population { get; set; }
 
-        //[JsonProperty("Latitude")]
         public double Latitude { get; set; }
 
-        //[JsonProperty("Longitude")]
         public double Longitude { get; set; }
     }
     
