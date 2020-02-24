e5f5fdd5fb90fd34347bcc176e2b2a42d6a8d05a
Maddy Leger <maddy@Maddys-MacBook-Pro.local>
2020-02-24 06:42:45 -0800

    start, no watch or tv

--- MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
-
 using System.Linq;
 using MonkeyFinder.Model;
 using System.Diagnostics;
@@ -14,18 +13,15 @@ namespace MonkeyFinder.ViewModel
 {
     public class MonkeysViewModel : BaseViewModel
     {
-        Random random = new Random();
         public Command GetMonkeysCommand { get; }
         public ObservableCollection<Monkey> Monkeys { get; }
+
         public MonkeysViewModel()
         {
             Title = "Monkey Finder";
             Monkeys = new ObservableCollection<Monkey>();
             GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
         }
-
-        HttpClient httpClient;
-        HttpClient Client => httpClient ?? (httpClient = new HttpClient());
 
         async Task GetMonkeysAsync()
         {
@@ -37,25 +33,7 @@ namespace MonkeyFinder.ViewModel
                 IsBusy = true;
                 Monkey[] monkeys = null;
 
-                var connection = DeviceInfo.Platform == DevicePlatform.watchOS ?
-                    NetworkAccess.Internet : Connectivity.NetworkAccess;
-
-                // if internet is working
-                if (connection == NetworkAccess.Internet)
-                {
-                    var json = await Client.GetStringAsync("https://montemagno.com/monkeys.json");
-
-                    monkeys = Monkey.FromJson(json);
-                }
-                else
-                {
-                    monkeys = new Monkey[]
-                    {
-                        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" },
-                        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" },
-                        new Monkey { Name = "Sample Monkey", Location = "Sample Monkey" }
-                    };
-                }
+                
 
                 Monkeys.Clear();
                 foreach (var monkey in monkeys)
@@ -73,18 +51,6 @@ namespace MonkeyFinder.ViewModel
             {
                 IsBusy = false;
             }
-        }
-
-        public async Task<string> GetRandomMonkey()
-        {
-            if (Monkeys.Count == 0)
-                await GetMonkeysAsync();
-
-            if (Monkeys.Count == 0)
-                return string.Empty;
-
-            var next = random.Next(0, Monkeys.Count);
-            return Monkeys[next].Image;
         }
     }
 }
